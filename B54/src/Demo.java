7986262de83a7b9b46521021cbad8c7f56dc7722
sairaj307 <66713555+sairaj307@users.noreply.github.com>
2023-05-03 10:44:50 +0530

    Add files via upload

---
+++ B54/src/Demo.java
@@ -0,0 +1,18 @@
+
+/**
+ * @author SAi
+ *
+ */
+/**
+ * @author SAi
+ *
+ */
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("LOVE YOU SAI");
+
+	}
+
+}
