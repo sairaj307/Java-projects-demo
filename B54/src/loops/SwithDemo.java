8f75f0292e0e4f0d102bdecd62b2c1cd646eae31
sairaj307 <66713555+sairaj307@users.noreply.github.com>
2023-05-09 16:30:09 +0530

    Add files via upload

---
+++ B54/src/loops/SwithDemo.java
@@ -0,0 +1,48 @@
+/**
+
+ * 
+ */
+
+package loops;
+import java.util.Scanner;
+/**
+ * @author SAi
+ *
+ */
+public class SwithDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		char x = input.next().charAt(0);
+		//char x = '1';
+		switch(x)
+		{
+		case 'l':
+		case 'L':
+		System.out.println(x+" is a Letter");
+		break;
+		case 'd':
+		case 'D':
+		System.out.println(x+" is a Digit");
+		break;
+		case 'w':
+		case 'W':
+		System.out.println(x+" is White Space");
+		break;
+		case 's':
+		case 'S':
+		System.out.println(x+" is Special Symbol");
+		default:
+		System.out.println(x+" is other than letter, digit, space or special symbol ");
+		break;
+
+		}
+
+	}
+
+}
