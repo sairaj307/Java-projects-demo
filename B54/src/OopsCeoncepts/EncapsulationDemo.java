a2b91039270323cf5f832576e734a3ae13977c99
sairaj307 <66713555+sairaj307@users.noreply.github.com>
2023-05-11 16:31:38 +0530

    Add files via upload

---
+++ B54/src/OopsCeoncepts/EncapsulationDemo.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package OopsCeoncepts;
+
+/**
+ * @author SAi
+ *
+ */
+public class EncapsulationDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OopsConceptDemo obj= new OopsConceptDemo(); 
+		obj.setSerialNum(101);
+		obj.setName("Pooja"); 
+		obj.setAge(20);
+		System.out.println(obj);
+
+	}
+
+}
---
+++ B54/src/OopsCeoncepts/OopsConceptDemo.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+package OopsCeoncepts;
+
+/**
+ * @author SAi
+ *
+ */
+/**
+ * 
+ */
+
+public class OopsConceptDemo {
+
+	/**
+	 * @param args
+	 */
+		// TODO Auto-generated method stub
+		private int serialNum;
+		private String name;
+		private int age;
+		int num;
+		
+		public int getSerialNum(){
+			return serialNum;
+		}
+		public void setSerialNum (int serialNum) { 
+			this.serialNum = serialNum;
+		}
+		public String getName() {
+		return name;
+		}
+		public void setName(String name) {
+		this.name = name;
+		}
+		public int getAge(){
+		return age;
+		}
+		public void setAge (int age) { 
+			this.age=age;
+		}
+		
+		
+		@Override
+		public String toString() {
+		return "OopsConcept Demo [Serial Number=" + serialNum+", name="+name+", age="+age + "]";
+		}
+
+
+		public static void main(String args[]) { 
+			//System.out.println(num); 
+			OopsConceptDemo obj= new OopsConceptDemo(); 
+			System.out.println(obj.serialNum);
+			System.out.println(obj.name);
+			//System.out.println(obj.num);
+		}
+		
+}
+
