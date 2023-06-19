9049dd8ca097b22f9d28cad1ed2544a00a14071d
sairaj307 <66713555+sairaj307@users.noreply.github.com>
2023-06-19 22:01:29 +0530

    Update RotateArray.java

--- B54/src/com/example/array/RotateArray.java
+++ B54/src/com/example/array/RotateArray.java
@@ -2,24 +2,39 @@ package com.example.array;
 
 public class RotateArray {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		int[] a = {10,20,30,40,50};
+	static int[] anti_Clockwise_Rotate(int[] a){
 		int t = a[0];
 		for(int i=0; i<a.length-1; i++){
 			a[i] = a[i+1];
 		}
 		a[a.length-1] = t;
-		
+		return a;
+		}
+	static int[] clockwise_Rotate(int[] a){
+		int t1 = a[a.length-1];
+		for(int i=a.length-1; i>0; --i){
+			a[i] = a[i-1];
+		}
+		a[0] = t1;
+		return a;
+		}
+	static void print(int[] a){
 		for(int i=0; i<a.length; i++){
 			System.out.print(a[i]+" ");
 		}
-		/*int t1 = a[0];
-		for(int i=0; i<a.length-1; i++){
-			a[i+1] = a[i];
-		}
-		a[a.length-1] = t;*/
+		System.out.println();
+	}
+	public static void main(String[] args) {
+		int[] a = {10,20,30,40,50};
+		System.out.println("Array Before Rotation:");
+		print(a);
+		anti_Clockwise_Rotate(a);
+		System.out.println("Array After Anti-clockwise Rotation:");
+		print(a);
+		
+		clockwise_Rotate(a);
+		System.out.println("Array After Clockwise Rotation:");
+		print(a);
 	}
 
 }
